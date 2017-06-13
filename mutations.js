a0d70e1e8b92ad9b05893765ed1eb4f0dae3dd60
Aditya Soni <adist98@gmail.com>
2017-06-14 02:15:08 +0530

    Create mutations.js

---
+++ mutations.js
@@ -0,0 +1,14 @@
+function mutation(arr) {
+  var test = arr[1].toLowerCase().split('');
+  var target = arr[0].toLowerCase().split('');
+  for (var i=0;i<test.length;i++) {
+    if (target.indexOf(test[i]) < 0)
+      return false;
+  }
+  return true;
+  
+
+}
+
+mutation(["hello", "hey"]);
+/* FCC CODE */
